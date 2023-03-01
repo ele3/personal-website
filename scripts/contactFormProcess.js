8ac90b423ce9ec3459bbe821e2d2b002a3a64580
ele3 <69780792+ele3@users.noreply.github.com>
2023-03-01 09:48:58 -0600

    Update contactFormProcess.js

--- scripts/contactFormProcess.js
+++ scripts/contactFormProcess.js
@@ -2,6 +2,9 @@ function contactFormProcess()
 {
     var contactFormObj = document.getElementById("contactForm");
 
+    console.log("Success");
+    console.log(contactFormObj.email.value);
+
     if (contactFormValidate(contactFormObj)) {
         alert("Valid Email!");
     } else {
